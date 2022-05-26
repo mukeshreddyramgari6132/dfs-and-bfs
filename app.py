dad983c3c7e4ae104e1798cefcd16d40778d22f6
mukeshreddyramgari6132 <99046551+mukeshreddyramgari6132@users.noreply.github.com>
2022-05-26 17:37:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+def bfs(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+bfs(graph, 'A')
+
+
+def dfs(graph, start):
+    visited, stack = set(), [start] # ❌ Because set() is not ordered, the order of node visits is lost.
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+dfs(graph, 'A') 
